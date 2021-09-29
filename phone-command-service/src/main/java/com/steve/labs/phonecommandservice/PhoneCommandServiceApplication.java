d267ed2961a87d7e6f07ec2e97f2c950a5efa3d3
Stephen Donlon <stephen.donlon@vodafone.com>
2021-09-29 15:22:27 +0100

    Refactoring name of phone-service and cleaning some test names. Added command generating project.

---
+++ phone-command-service/src/main/java/com/steve/labs/phonecommandservice/PhoneCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.steve.labs.phonecommandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneCommandServiceApplication.class, args);
+	}
+
+}
---
+++ phone-command-service/src/test/java/com/steve/labs/phonecommandservice/PhoneCommandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.labs.phonecommandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneCommandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
